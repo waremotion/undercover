ecda9917eb6e1283ee37e28754c2f75680b8a3ce
waremotion <waremotion@gmail.com>
2026-08-05 00:35:00 +0200

    Actualise le cache pour la version 8

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="undercover-v7-1";
+const CACHE_NAME="undercover-v8-1";
 const ASSETS=["./","./index.html","./styles.css","./words.js","./app.js","./manifest.webmanifest"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))));self.clients.claim()});
