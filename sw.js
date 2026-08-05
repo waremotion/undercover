8d0cd60138d77b5bddc9fb5703f202452be3f4f5
waremotion <waremotion@gmail.com>
2026-08-05 20:59:16 +0200

    Actualise le cache pour la session persistante

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME="undercover-v8-1";
-const ASSETS=["./","./index.html","./styles.css","./words.js","./app.js","./manifest.webmanifest"];
+const CACHE_NAME="undercover-v9-1";
+const ASSETS=["./","./index.html","./styles.css","./session.css","./words.js","./app.js","./manifest.webmanifest"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))));self.clients.claim()});
 self.addEventListener("fetch",event=>{if(event.request.method!=="GET")return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match("./index.html"))))});
